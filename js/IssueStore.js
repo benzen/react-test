ed6c83fc52110d7510619b8d708b2fc8a0911d9f
Alexandre Rimthong <rimthong@gmail.com>
2015-02-13 11:19:31 -0500

    Added store!

---
+++ js/IssueStore.js
@@ -0,0 +1,31 @@
+var EventEmitter = require('events'),
+  _ = require('underscore');
+
+var CHANGE_EVENT = 'changed';
+
+var IssueStore = _.extend({}, EventEmitter.prototype, {
+
+  issues: [
+    {id:1, title:"YO meeen", code:"REACT-1", reporter:"Benjamin Dreux", status:"TODO", description:"WTF", labels:"ASAP"},
+    {id:2, title:"YO meeen", code:"REACT-2", reporter:"Benjamin Dreux", status:"TODO", description:"WTF", labels:"ASAP"},        
+  ], 
+
+  emitChange: function() {
+    this.emit(CHANGE_EVENT);
+  },
+
+  addChangeListener: function(callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+
+  removeChangeListener: function(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  },
+
+  getIssues: function(){
+    return this.issues
+  }
+
+});
+
+module.exports = IssueStore;
--- js/app.js
+++ js/app.js
@@ -2,13 +2,8 @@ var React  = require("react");
 var IssueList = require("./IssueList");
 var NewIssueForm = require("./NewIssueForm");
 
-var issues = [
-  {id:1, title:"YO meeen", code:"REACT-1", reporter:"Benjamin Dreux", status:"TODO", description:"WTF", labels:"ASAP"},
-  {id:2, title:"YO meeen", code:"REACT-2", reporter:"Benjamin Dreux", status:"TODO", description:"WTF", labels:"ASAP"},        
-];
-
 React.render(
-  React.createElement(IssueList, {issues: issues}),
+  React.createElement(IssueList),
   document.getElementById('issueList')
 );
 
