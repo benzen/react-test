0e1dc865c5dea7468a75f20b74e1e7682a97f55e
Benjamin Dreux <benjamin.dreux@code3.ca>
2015-02-14 08:12:36 -0500

    correct usage of _.extend

--- js/IssueDispatcher.js
+++ js/IssueDispatcher.js
@@ -1,6 +1,4 @@
 var _ = require("underscore");
 var Dispatcher = require("flux").Dispatcher;
-
-var IssueDispatcher = _.extend( new Dispatcher(), {});
-
+var IssueDispatcher = _.extend( {}, new Dispatcher());
 module.exports = IssueDispatcher;  
