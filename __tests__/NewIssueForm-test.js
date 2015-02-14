740b45e11b12320847c4d4c5db240a2e844674ae
Benjamin Dreux <benjamin.dreux@code3.ca>
2015-02-14 17:21:32 -0500

    augment tests

--- __tests__/NewIssueForm-test.js
+++ __tests__/NewIssueForm-test.js
@@ -11,7 +11,7 @@ describe('NewIssueForm', function() {
     NewIssueForm = require(moduleUnderTest);
   });
 
-  it("is true", function() {
+  it("has a button", function() {
     var form = TestUtils.renderIntoDocument(
       <NewIssueForm/>
     );
@@ -22,4 +22,35 @@ describe('NewIssueForm', function() {
 
     }
   );
+  it("has 5 input", function() {
+    var form = TestUtils.renderIntoDocument(
+      <NewIssueForm/>
+    );
+    var inputs = TestUtils.scryRenderedDOMComponentsWithTag(form, 'input');
+
+    expect(inputs.length).toBe(6)
+
+  });
+  describe("when clicking on the button", function(){
+    var IssueActionCreator, IssueStore;
+
+    beforeEach(function(){
+      IssueActionCreator = require("../js/IssueActionCreator");
+      IssueStore = require("../js/IssueStore");
+      IssueStore.getIssues.mockReturnValue({length:0});      
+    });
+    
+    it("it calls action creator with the new issue", function() {
+
+      var form = TestUtils.renderIntoDocument(
+        <NewIssueForm/>
+      );
+    
+      var button = TestUtils.findRenderedDOMComponentWithTag(form, 'button');
+      TestUtils.Simulate.submit(button)
+      
+      expect(IssueActionCreator.createNewIssue.mock.calls.length).toBe(1)
+
+    });
+  });
 });
